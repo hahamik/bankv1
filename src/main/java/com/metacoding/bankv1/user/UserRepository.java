6b407158e886a5fd3976942f4a04ef58ef2a0116
hahamik <dlapdl0615@gmail.com>
2025-03-24 17:40:17 +0900

    User findByUsername & save

---
+++ src/main/java/com/metacoding/bankv1/user/UserRepository.java
@@ -0,0 +1,36 @@
+package com.metacoding.bankv1.user;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor // 파이널이 붙은 애들로 생성자를 만들어줌
+@Repository
+public class UserRepository {
+
+    private final EntityManager em; // 파이널이 붙으면 생성할 때마다 초기화를 해줘야 한다.
+
+    public void save() {
+
+    }
+
+    public User findByUsername(String username) {
+        Query query = em.createNativeQuery("SELECT * FROM user_tb WHERE username = ?", User.class);
+        query.setParameter(1, username);
+        try {
+            return (User) query.getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void save(String username, String password, String fullname) {
+        Query query = em.createNativeQuery("INSERT INTO user_tb(username, password,fullname,created_at)VALUES(?,?,?,now())");
+        query.setParameter(1, username);
+        query.setParameter(2, password);
+        query.setParameter(3, fullname);
+        query.executeUpdate();
+    }
+
+}
---
+++ src/main/java/com/metacoding/bankv1/user/UserRequest.java
@@ -0,0 +1,23 @@
+package com.metacoding.bankv1.user;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+public class UserRequest {
+
+    @Data
+    public static class JoinDTO {
+        private String username;
+        private String password;
+        private String fullname;
+        private Timestamp createdAt;
+    }
+
+    @Data
+    public static class LoginDTO {
+        private String username;
+        private String password;
+    }
+
+}
